f6b0d6f9f1dcffb2aebb2ea4fd5e66a53b93199e
Idan Ariav <idanaok@proton.me>
2025-04-24 14:56:08 +0300

    fix profile image url

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
@@ -18,7 +19,7 @@ function HomepageHeader() {
       <div className="container text--center">
         {/* Professional headshot */}
         <img
-          src="/general/profile_photo.jpg"
+          src={useBaseUrl('/general/profile_photo.jpg')}
           alt="Professional Headshot"
           className={styles.headshot}
         />
