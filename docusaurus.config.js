d60703650fc5bbccfacef6f727b903af8dfc230e
Idan Ariav <idanaok@proton.me>
2025-05-02 14:34:58 +0300

    change broken links to warning

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
   projectName: 'personal-website-docusaurus', // Usually your repo name.
   deploymentBranch: 'gh-pages',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
